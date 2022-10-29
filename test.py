6c3bf77a05672dbd7b5b1fbfd00be38454f02220
Ifechukwudeni Oweh <teddyoweh@gmail.com>
2022-10-28 17:29:25 -0700

    Create test.py

---
+++ test.py
@@ -0,0 +1,6 @@
+import pyshark
+
+capture = pyshark.LiveCapture(interface='eth0')
+capture.sniff(timeout=50)
+for packet in capture.sniff_continuously(packet_count=5):
+    print('Just arrived:', packet)
