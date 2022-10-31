c4d9a0c5ff23545574ada5875c73e4804f7cf1e3
Ifechukwudeni Oweh <teddyoweh@gmail.com>
2022-10-31 08:53:34 -0700

    update

--- scanner.py
+++ scanner.py
@@ -1,53 +1,27 @@
-from scapy.all import *
-from scapy.layers.http import HTTPRequest # import HTTP packet
-from colorama import init, Fore
-# initialize colorama
-init()
-# define colors
-GREEN = Fore.GREEN
-RED   = Fore.RED
-RESET = Fore.RESET
-def sniff_packets(iface=None):
-    """
-    Sniff 80 port packets with `iface`, if None (default), then the
-    Scapy's default interface is used
-    """
-    if iface:
-        # port 80 for http (generally)
-        # `process_packet` is the callback
-        sniff(filter="port 80", prn=process_packet, iface=iface, store=False)
-    else:
-        # sniff with default interface
-        sniff(filter="port 80", prn=process_packet, store=False)        
+from scapy.all import ARP, Ether, srp
 
+target_ip = "10.119.144.43"
+# IP Address for the destination
+# create ARP packet
+arp = ARP(pdst=target_ip)
+# create the Ether broadcast packet
+# ff:ff:ff:ff:ff:ff MAC address indicates broadcasting
+ether = Ether(dst="ff:ff:ff:ff:ff:ff")
+# stack them
+packet = ether/arp
 
-def process_packet(packet):
-    """
-    This function is executed whenever a packet is sniffed
-    """
-    if packet.haslayer(HTTPRequest):
-        # if this packet is an HTTP Request
-        # get the requested URL
-        url = packet[HTTPRequest].Host.decode() + packet[HTTPRequest].Path.decode()
-        # get the requester's IP Address
-        ip = packet[IP].src
-        # get the request method
-        method = packet[HTTPRequest].Method.decode()
-        print(f"\n{GREEN}[+] {ip} Requested {url} with {method}{RESET}")
-        if show_raw and packet.haslayer(Raw) and method == "POST":
-            # if show_raw flag is enabled, has raw data, and the requested method is "POST"
-            # then show raw
-            print(f"\n{RED}[*] Some useful Raw data: {packet[Raw].load}{RESET}")
+# srp() function sends and receives packets at layer 2
+result = srp(packet, timeout=3, verbose=0)[0]
 
+# a list of clients, we will fill this in the upcoming loop
+clients = []
 
-if __name__ == "__main__":
-    import argparse
-    parser = argparse.ArgumentParser(description="HTTP Packet Sniffer, this is useful when you're a man in the middle." \
-                                                 + "It is suggested that you run arp spoof before you use this script, otherwise it'll sniff your personal packets")
-    parser.add_argument("-i", "--iface", help="Interface to use, default is scapy's default interface")
-    parser.add_argument("--show-raw", dest="show_raw", action="store_true", help="Whether to print POST raw data, such as passwords, search queries, etc.")
-    # parse arguments
-    args = parser.parse_args()
-    iface = args.iface
-    show_raw = args.show_raw
-    sniff_packets(iface)
+for sent, received in result:
+    # for each response, append ip and mac address to `clients` list
+    clients.append({'ip': received.psrc, 'mac': received.hwsrc})
+
+# print clients
+print("Available devices in the network:")
+print("IP" + " "*18+"MAC")
+for client in clients:
+    print("{:16}    {}".format(client['ip'], client['mac']))
